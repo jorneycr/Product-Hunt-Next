9a376897d564863e86d14ed8b9105674127fbb2a
jorney Lopez <jorney95@live.com>
2021-10-24 22:17:41 -0600

    structure

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -2,11 +2,46 @@ import React from 'react';
 import Layout from '../components/layouts/Layout';
 
 const CrearCuenta = () => {
-    return ( 
-        <Layout>
+  return (
+    <Layout>
+      <>
         <h1>Crear Cuenta</h1>
-      </Layout>
-     );
+        <form>
+          <div>
+            <label htmlFor="nombre">Nombre</label>
+            <input
+              type="text"
+              id="nombre"
+              placeholder="Tu nombre"
+              name="nombre"
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Tu email"
+              name="email"
+            />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Tu password"
+              name="password"
+            />
+          </div>
+          <input 
+            type="submit"
+            value="Crear Cuenta"
+          />
+        </form>
+      </>
+    </Layout>
+  );
 }
- 
+
 export default CrearCuenta;
