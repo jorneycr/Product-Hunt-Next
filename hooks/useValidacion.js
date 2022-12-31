ebdfee1c2d356483f8d77501d1dec6d03868981a
jorneylopez <jorney95@live.com>
2022-12-30 22:51:11 -0600

    Done hook

--- hooks/useValidacion.js
+++ hooks/useValidacion.js
@@ -1,9 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const [state, setState] = useState([]);
+const useValidacion = (stateInicial, validar, fn) => {
+  const [valores, guardarValores] = useState(stateInicial);
+  const [errores, guardarErrores] = useState({});
+  const [submitForm, guardarSubmitForm] = useState(false);
 
-const useValidacion = () => {
-  return <div>useValidacion</div>;
+  useEffect(() => {
+    if (submitForm) {
+      const noErrores = Object.keys(errores).length === 0;
+
+      if (noErrores) {
+        fn();
+      }
+      guardarSubmitForm(false);
+    }
+  }, []);
+
+  const handleChange = (e) => {
+    guardarValores({
+      ...valores,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const erroresValidacion = validar(valores);
+    guardarErrores(erroresValidacion);
+    guardarSubmitForm(true);
+  };
+
+  return {
+    valores,
+    errores,
+    submitForm,
+    handleSubmit,
+    handleChange,
+  };
 };
 
 export default useValidacion;
