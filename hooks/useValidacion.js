eb64c0f57f0b0cb9edac39b6a22f9fbe6b777c29
jorneylopez <jorney95@live.com>
2022-12-30 22:45:24 -0600

    Added a new hook

---
+++ hooks/useValidacion.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const [state, setState] = useState([]);
+
+const useValidacion = () => {
+  return <div>useValidacion</div>;
+};
+
+export default useValidacion;
