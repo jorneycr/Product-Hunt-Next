53271624a62c614e03217086f3a445dbb741a9db
jorneylopez <jorney95@live.com>
2022-12-31 23:57:36 -0600

    changes

--- components/ui/Boton.jsx
+++ components/ui/Boton.jsx
@@ -9,7 +9,7 @@ const Boton = styled.a`
     text-align: center;
     background-color: ${props => props.bgColor ? '#DA552F' : 'white'};
     color: ${props => props.bgColor ? 'white' : '#000'};
-    &:last-of-type {
+    &:first-of-type {
         margin-right:0;
     }
     &:hover {
