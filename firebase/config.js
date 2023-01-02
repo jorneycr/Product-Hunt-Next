f7ecec6149794ecc282686dade41b81b25b0ec2b
jorneylopez <jorney95@live.com>
2023-01-01 22:58:15 -0600

    Added firebase config files

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBrhJk69Xj1wno2ZCn02RpGgsAMJ4zacQ",
+  authDomain: "product-hunt-e74e1.firebaseapp.com",
+  projectId: "product-hunt-e74e1",
+  storageBucket: "product-hunt-e74e1.appspot.com",
+  messagingSenderId: "869076891058",
+  appId: "1:869076891058:web:a661c5ea7e230c286860ab",
+};
+
+export default firebaseConfig;
