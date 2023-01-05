a59b2454d4e0f0ff3a0eeb4d4c032427202490e5
jorneylopez <jorney95@live.com>
2023-01-04 18:12:21 -0600

    Added firebase config

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { BreakpointProvider } from "react-socks";
+import firebase, { FirebaseContext } from "../firebase";
 
 export default function App({ Component, pageProps }) {
   const [showChild, setShowChild] = useState(false);
@@ -12,8 +13,14 @@ export default function App({ Component, pageProps }) {
     return null;
   }
   return (
-    <BreakpointProvider>
-      <Component {...pageProps} />
-    </BreakpointProvider>
+    <FirebaseContext.Provider
+      value={{
+        firebase,
+      }}
+    >
+      <BreakpointProvider>
+        <Component {...pageProps} />
+      </BreakpointProvider>
+    </FirebaseContext.Provider>
   );
 }
